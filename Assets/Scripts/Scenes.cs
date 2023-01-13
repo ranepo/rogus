316cbe77401437fd26f50a543a9aaedf4821c3fb
Okapi12345 <eemeli.vepsalainen@gmail.com>
2023-01-13 19:40:29 +0200

    commit

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject DeathMenuUI;
+    void Start()
+    {
+        //PlayerPrefs.SetInt("Level", 1);
+        //Debug.Log("moi");
+        //Debug.Log(PlayerPrefs.GetInt("Level"));
+        //SceneManager.LoadScene(PlayerPrefs.GetInt("Level", 1));
+ 
+    }
+
+    public void LoadNewScene()
+    {
+        PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        DeathMenuUI.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("Level", 1));
+    }
+    public void RunTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
